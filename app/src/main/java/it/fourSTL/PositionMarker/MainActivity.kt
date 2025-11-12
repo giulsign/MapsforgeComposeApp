8a35bd1f54ef9148387f61ea79854fdfed89bf4e
giuliano 3d <giulisign@hotmail.com>
2025-11-12 20:14:42 +0100

    12 11 25 - reso visibile barra di istema superiore dell'app - aggiunto jason tartufi

--- app/src/main/java/it/fourSTL/PositionMarker/MainActivity.kt
+++ app/src/main/java/it/fourSTL/PositionMarker/MainActivity.kt
@@ -11,24 +11,28 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // ✅ Disattiva il "fit" delle system bars
-        WindowCompat.setDecorFitsSystemWindows(window, false)
 
-        // ✅ Controller per gestire le barre
-        val controller = WindowInsetsControllerCompat(window, window.decorView).apply {
-            // Le barre riappaiono solo con swipe dal bordo
-            systemBarsBehavior =
-                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-            // Nasconde subito status bar + navigation bar
-            hide(WindowInsetsCompat.Type.systemBars())
-        }
+        // ✅ Imposta che il contenuto rispetti gli spazi di status e navigation bar
+        WindowCompat.setDecorFitsSystemWindows(window, true)
 
-
+        // Abilita la visualizzazione edge-to-edge
+        enableEdgeToEdge()
 
         setContent {
             MaterialTheme {
@@ -39,4 +43,21 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
+/*
+@Composable
+fun MapsforgeMapComposable() {
+    // Aggiunge padding per rispettare le barre di sistema
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFF2F2F2))
+            .padding(WindowInsets.systemBars.asPaddingValues())
+    ) {
+        Text(
+            text = "Contenuto dell’app",
+            fontSize = 20.sp,
+            color = Color.Black,
+            modifier = Modifier.align(Alignment.Center)
+        )
+    }
+}*/
