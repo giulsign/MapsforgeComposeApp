40945454b9fc2d4b70da825d7ff15485705c85cb
giuliano 3d <giulisign@hotmail.com>
2025-12-22 19:10:44 +0100

    22 12 25
    - aggiunte funzionalità a gpxtrack menu
    - creata nuova categoria comandi Personalized metadatas per importazione metadati esterni
    - aggiunto impostazione base group sharing via bluetooth e server http da device host fino a max 15 elementi per gruppo - codice funzionante da testare con più dispositivi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,18 @@
 plugins {
     id("com.android.application")
     kotlin("android")
-    id("com.mikepenz.aboutlibraries.plugin") // <--
+    id("com.mikepenz.aboutlibraries.plugin")
+    kotlin("plugin.serialization")// version "1.9.25"
 }
 
 android {
     namespace = "it.fourSTL.PositionMarker"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "it.fourSTL.PositionMarker"
         minSdk = 27 //  Min API level for Google Maps SDK
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "0.1.0"
     }
@@ -47,11 +48,11 @@ dependencies {
 
     implementation("androidx.core:core-splashscreen:1.0.1")
 
-    implementation(platform("androidx.compose:compose-bom:2025.09.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.09.00"))
 
 
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation("androidx.activity:activity-compose:1.9.3")
 
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material:material")
@@ -67,11 +68,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material:material-icons-extended:1.7.5")
 
-
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-
-
-    implementation("com.google.android.gms:play-services-location:21.1.0")
+    //implementation("com.google.android.gms:play-services-location:21.1.0")
 
 
     implementation("org.mapsforge:mapsforge-map-android:0.25.0")
@@ -89,12 +86,12 @@ dependencies {
     implementation("com.caverock:androidsvg:1.4")
 
 
-    implementation ("androidx.appcompat:appcompat:1.6.1")
+    implementation ("androidx.appcompat:appcompat:1.7.0")
     implementation ("androidx.recyclerview:recyclerview:1.3.2")
 
     implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
 
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
 
     // AboutLibraries - For open source licenses automatic compilation
     implementation("com.mikepenz:aboutlibraries-compose:11.1.0")
@@ -102,8 +99,13 @@ dependencies {
 
     // libraries for group sharing
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
     implementation("org.java-websocket:Java-WebSocket:1.5.4")
-    implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 
+    // Serializzazione JSON
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+
+    // Coroutines (potrebbero essere già presenti)
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
 }
